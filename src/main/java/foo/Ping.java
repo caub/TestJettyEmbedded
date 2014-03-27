51dcdd1523ee8513422ade1d85ff2b86259dfd9c
unknown <cyril.auburtin@gmail.com>
2014-03-27 17:39:42 +0100

    pom updates

--- src/main/java/foo/Ping.java
+++ src/main/java/foo/Ping.java
@@ -1,29 +1,23 @@
 package foo;
 
-import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-/**
- * Servlet implementation class Ping
- */
-@WebServlet("/ping")
+
+@WebServlet("/pingfoobar")
 public class Ping extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
+
     public Ping() {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getOutputStream().print("PONG");
